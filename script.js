088c477e6d4dceffefde22b2e392f8ff653bd15c
lanniiee <lav250397@hotmail.com>
2022-10-04 23:12:47 +0100

    added in search function to filter by name

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
+//import pokemonArray from pokemon.js
 import defaultExport, { pokemonArray } from "./data/pokemon.js"
 
+
+// Selecting element from HTML
 const cardContainer = document.querySelector(".card-container");
 const searchInput = document.getElementById("search");
 
-let result = [];
-
-
+// added in card container to show all objects from array using forEach
 pokemonArray.forEach((pokemon) => {
 const name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
 const types = pokemon.types.join(" & ");
@@ -19,5 +20,27 @@ cardContainer.innerHTML +=
     </div>`;
 });
 
+// filter pokemon by name. At the moment only exact match
+const filterPokemon = () => {
+    let input = document.getElementById("search").value;
+    input = input.toLowerCase();
+    const clearCard = document.querySelectorAll(".card");
+    clearCard.forEach(card => card.remove());
 
+    pokemonArray.forEach((pokemon) => {
+        const name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+        const types = pokemon.types.join(" & ");
+        if (input == pokemon.name) {
+            cardContainer.innerHTML = 
+            `<div class="card">
+                <img src="${pokemon.sprite}" class="card__image" id="${name}">
+                <div class="card__content">
+                    <div class="card__heading">${name}</div>
+                <div class="card__text">${name} (#${pokemon.id}) is a ${types} type pokemon.</div>
+                </div>
+            </div>`;
+        }
+    })
+}
 
+searchInput.addEventListener("input", filterPokemon);
