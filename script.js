33d1639190e02daef1dc404329af5af533507338
lanniiee <lav250397@hotmail.com>
2022-10-04 23:32:32 +0100

    added in search for type

--- script.js
+++ script.js
@@ -23,14 +23,15 @@ cardContainer.innerHTML +=
 // filter pokemon by name. At the moment only exact match
 const filterPokemon = () => {
     let input = document.getElementById("search").value.toLowerCase();
-    const clearCard = document.querySelectorAll(".card");
-    clearCard.forEach(card => card.remove());
+    const card = document.querySelectorAll(".card");
+    card.forEach(card => card.remove());
 
     pokemonArray.forEach((pokemon) => {
         const name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
         const types = pokemon.types.join(" & ");
+        let result = [];
         if (input == pokemon.name) {
-            cardContainer.innerHTML = 
+            cardContainer.innerHTML += 
             `<div class="card">
                 <img src="${pokemon.sprite}" class="card__image" id="${name}">
                 <div class="card__content">
@@ -39,6 +40,16 @@ const filterPokemon = () => {
                 </div>
             </div>`;
         }
+        if (input == pokemon.types[0] || input == pokemon.types[1]) {
+            cardContainer.innerHTML += 
+            `<div class="card">
+                <img src="${pokemon.sprite}" class="card__image" id="${name}">
+                <div class="card__content">
+                    <div class="card__heading">${name}</div>
+                <div class="card__text">${name} (#${pokemon.id}) is a ${types} type pokemon.</div>
+                </div>
+            </div>`;
+        };
     })
 }
 
