4f9d2594b3fee07a9925327457521d9ec4526fb1
lanniiee <lav250397@hotmail.com>
2022-10-04 15:22:21 +0100

    added in & sign for type

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ const cardContainer = document.querySelector(".card-container");
 
 
 pokemonArray.forEach((pokemon) => {
-    console.log(pokemon.types);
+    const name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+    const types = pokemon.types.join(" & ");
+    console.log(types);
     cardContainer.innerHTML +=
     `<div class="card">
             <img src="${pokemon.sprite}" class="card__image">
             <div class="card__content">
-                <div class="card__heading">${pokemon.name}</div>
-                <div class="card__text">${pokemon.name} (#${pokemon.id}) is a ${pokemon.types} type pokemon.</div>
+                <div class="card__heading">${name}</div>
+                <div class="card__text">${name} (#${pokemon.id}) is a ${types} type pokemon.</div>
             </div>
     </div>`;
 });
