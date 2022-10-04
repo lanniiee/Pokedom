8f73b574d4a89ee4aa7a130f1ff0ee2c2d52aed8
lanniiee <lav250397@hotmail.com>
2022-10-04 20:47:57 +0100

    give each card an id

--- script.js
+++ script.js
@@ -5,21 +5,19 @@ const searchInput = document.getElementById("search");
 
 let result = [];
 
-const displayPokemon = () => {
-    pokemonArray.forEach((pokemon) => {
-    const name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-    const types = pokemon.types.join(" & ");
-    cardContainer.innerHTML +=
+
+pokemonArray.forEach((pokemon) => {
+const name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+const types = pokemon.types.join(" & ");
+cardContainer.innerHTML +=
     `<div class="card">
-            <img src="${pokemon.sprite}" class="card__image">
-            <div class="card__content">
-                <div class="card__heading">${name}</div>
-                <div class="card__text">${name} (#${pokemon.id}) is a ${types} type pokemon.</div>
-            </div>
+        <img src="${pokemon.sprite}" class="card__image" id="${name}">
+        <div class="card__content">
+            <div class="card__heading">${name}</div>
+            <div class="card__text">${name} (#${pokemon.id}) is a ${types} type pokemon.</div>
+        </div>
     </div>`;
 });
-};
 
-displayPokemon(pokemonArray);
 
 
